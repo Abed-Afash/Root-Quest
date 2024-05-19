c9546999f00f1ec16e50eb4e1258cf9a8dc9bd98
Abdalsamee Afash <abdalsamee.af@gmail.com>
2024-05-19 17:25:45 +0300

    added newton Raphson and made some improvements to the evaluation technique

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import FalsePosition from './components/FalsePosition'
 import Secant from './components/Secant'
 import Muller from './components/Muller'
 import Header from './components/Header'
+import NewtonRaphson from './components/NewtonRaphson'
+import {parse} from 'mathjs'
 function App() {
   const [mode, setMode] = React.useState("Bisection")
   const [expression, setExpression] = React.useState("")
@@ -12,31 +14,17 @@ function App() {
     const {value, type} = event.target
     type === "select-one" ? setMode(value) : setExpression(value)
   }
-  const transform = (expression) => {
-    expression = expression.replace(/\s/g, '')
-    expression = expression.replace(/\^/g, '**')
-    expression = expression.replace(/(\d)tan/g, '$1 * tan')
-    expression = expression.replace(/(\d)cos/g, '$1 * cos')
-    expression = expression.replace(/(\d)sin/g, '$1 * sin')
-    expression = expression.replace(/(\d)ln/g, '$1 * ln')
-    expression = expression.replace(/ln/g, 'Math.log')
-    expression = expression.replace(/sin/g, 'Math.sin')
-    expression = expression.replace(/cos/g, 'Math.cos')
-    expression = expression.replace(/tan/g, 'Math.tan')
-    expression = expression.replace(/e/g, 'Math.E')
-    expression = expression.replace(/(\d)x/g, '$1 * x')
-    return expression
-  }
   const calculateExpression = (expression, x) => {
-    expression = transform(expression)
-    expression = expression.replace(/x/g, `(${x})`)
+    expression = expression.replace(/ln/g, 'Math.log')
     try{
-        const result = eval(expression)
-        return result
-    } catch(error){
-        console.error(error)
-        return null
-    }
+      const node = parse(expression)
+      const scope = {x}
+      const result = node.evaluate(scope)
+      return result
+  } catch(error){
+      console.error(error)
+      return null
+  }
 }
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -51,6 +39,7 @@ function App() {
           <select onChange={handleChange} value={mode}>
             <option value="Bisection">Bisection</option>
             <option value="False Position">False position</option>
+            <option value="Newton Raphson">Newton Raphson</option>
             <option value="Secant">Secant</option>
             <option value="Muller">Muller</option>
           </select>
@@ -60,6 +49,7 @@ function App() {
           </div>
           {mode === "Bisection" && <Bisection calculateExpression ={calculateExpression} expression = {expression}/>}
           {mode === "False Position" && <FalsePosition calculateExpression ={calculateExpression} expression = {expression}/>}
+          {mode === "Newton Raphson" && <NewtonRaphson calculateExpression ={calculateExpression} expression = {expression}/>}
           {mode === "Secant" && <Secant calculateExpression ={calculateExpression} expression = {expression}/>}
           {mode === "Muller" && <Muller calculateExpression = {calculateExpression} expression = {expression}/>}
         </form>
---
+++ src/components/NewtonRaphson.js
@@ -0,0 +1,98 @@
+import React, {useState} from 'react'
+import {derivative} from 'mathjs'
+
+const NewtonRaphson = ({calculateExpression, expression}) => {
+    const [derivedExpression, setDerivedExpression] = useState('')
+    const [inputs, setInputs] = useState({
+        xi: '',
+        iterations: ''
+    })
+    const [table, setTable] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+    const handleChange = (event) => {
+        const {value, name} = event.target;
+        setInputs(prev =>({
+           ...prev,
+            [name]: value
+    }))}
+    const handleDerivative = (expression , x) => {
+        setDerivedExpression(derivative(expression, 'x').toString())
+        const derived = derivative(expression, 'x').toString()
+        console.log(derived)
+        return calculateExpression(derived, x)
+    }
+    const handleNewtonRaphson = () => {
+        setTable([])
+        const {xi, iterations} = inputs;
+        let xiTemp = parseFloat(xi);
+        const array = [];
+        for(let i = 0; i < iterations; i++){
+            let fxi = calculateExpression(expression, xiTemp);
+            let fxiDerivative = handleDerivative(expression, xiTemp);
+            let xiPlusOne = xiTemp - (fxi/fxiDerivative);
+            let fxiPlusOne = calculateExpression(expression, xiPlusOne);
+            if(fxi == null || fxiPlusOne == null){
+                setErrorMessage("Please enter a valid function")
+                setDerivedExpression('')
+                return
+            }
+            array.push({
+                iteration: i + 1,
+                xi: xiTemp,
+                fxi: fxi,
+                fxiDerivative: fxiDerivative,
+                xiPlusOne: xiPlusOne,
+                fxiPlusOne: fxiPlusOne
+            })
+            xiTemp = xiPlusOne;
+            if(fxiPlusOne === 0){
+                break;
+            }
+        }
+        setTable(array)
+        setErrorMessage('')
+    }
+    console.log(table)
+    const htmlTable = table.map((entry,index) => <tr key={index}>
+    <td>{entry.iteration}</td>
+    <td>{entry.xi !== null ? entry.xi.toFixed(3) : null}</td>
+    <td>{entry.fxi !== null ? entry.fxi.toFixed(3) : null}</td>
+    <td>{entry.fxiDerivative !== null ? entry.fxiDerivative.toFixed(3) : null}</td>
+    <td>{entry.xiPlusOne !== null ? entry.xiPlusOne.toFixed(3) : null}</td>
+    <td>{entry.fxiPlusOne !== null ? entry.fxiPlusOne.toFixed(3) : null}</td>
+</tr>)
+  return (
+    <div className='method-container'>
+        <div className="inputs-container">
+                <div>
+                    <label htmlFor="X(i)">X(i) = </label>
+                    <input placeholder="Enter X(i)" type='number' required name="xi" onChange={handleChange} value={inputs.xi}/>
+                </div>
+            </div>
+            <div className='submit-container'>
+                <label htmlFor = 'iterations'>Iterations: </label>
+                <input placeholder='How many iterations?' id='iterations' type='number' name="iterations" required onChange={handleChange} value={inputs.iterations}/>
+                <button onClick={handleNewtonRaphson}>Calculate</button>
+            </div>
+            {derivedExpression && <h1>Derivative: {derivedExpression}</h1>}
+            {table.length > 0 && <table className="table">
+                <thead>
+                    <tr>
+                        <th>Iteration</th>
+                        <th>X(i)</th>
+                        <th>f(Xi)</th>
+                        <th>f'(Xi)</th>
+                        <th>X(i+1)</th>
+                        <th>f(X(i+1))</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {htmlTable}
+                </tbody>
+            </table>}
+            {errorMessage.length > 0 && <h1>{errorMessage}</h1>}
+    </div>
+  )
+}
+
+export default NewtonRaphson
