669388ba37694f21fd309518b8396376de54434c
Abdalsamee Afash <abdalsamee.af@gmail.com>
2024-05-29 22:56:27 +0300

    added beta

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = ({category, setCategory}) => {
     return(
         <div className="header">
             <button className={category === 'root' ? 'modeButton' : ''} onClick={() => setCategory('root')}>RootQuest</button>
-            <button className={category === 'integral' ? 'modeButton' : ''} onClick={() => setCategory('integral')}>IntegralQuest</button>
+            <button id="integral-button" className={category === 'integral' ? 'modeButton' : ''} onClick={() => setCategory('integral')}>IntegralQuest</button>
         </div>
     )
 }
